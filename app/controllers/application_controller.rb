8490ba1f6af2b411bf4f2ca558c34517fb4123c7
patrickli90 <patrick.li@kellogg.northwestern.edu>
2026-02-20 20:34:16 +0000

    controller edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ApplicationRecord
-  
+
   # talks to the "entries" table
+
 end
