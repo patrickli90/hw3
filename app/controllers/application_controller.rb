6ae035158a6757354f6c7769ab3ee4b8606d10c0
patrickli90 <patrick.li@kellogg.northwestern.edu>
2026-02-20 21:14:41 +0000

    places controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
 
-  def index
-  end
 
-  def new
-  end
-
-  def show
-  end
-
-  def create
-  end
 end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+
+def index
+    @places = Place.all
+    render :template => "places/index"
+
+  end
+
+  def new
+    render :template => "places/new"
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    render :template => "places/show"
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
