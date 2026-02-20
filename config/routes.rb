24af9fca2ef5725df9c84cf1e08bec73d1f18a2e
patrickli90 <patrick.li@kellogg.northwestern.edu>
2026-02-20 20:14:29 +0000

    route edits

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+  post("/places", { :controller => "places", :action => "create" })
+
+  get("/entries/new", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" })
+
 end
